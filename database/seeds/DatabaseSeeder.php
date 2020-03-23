1faa989a797f0b95c122e3ff5eba43b94bb19f4f
Hrant-p <hrant@live.ru>
2020-03-24 00:36:42 +0400

    added seeds and factories

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Category;
+use App\Product;
+use App\Transaction;
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         // $this->call(UsersTableSeeder::class);
+        User::truncate();
+        Category::truncate();
+        Product::truncate();
+        Transaction::truncate();
+        DB::table('category_product')->truncate();
+
+        $userQuantity = 200;
+        $categoriesQuantity = 30;
+        $productQuantity = 1000;
     }
 }
